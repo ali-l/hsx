4a752834488e22fa54ac74411c9b8e7f90f25002
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-03-10 15:42:29 -0500

    Read stream record and fetch html for bond

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+import {fetch} from './utils'
+
 // noinspection JSUnusedGlobalSymbols
-export default (event, context, callback) => {
-	callback(null, 'Hello world')
+export default ({ Records }, context, callback) => {
+  const BASE_URL = 'https://www.hsx.com/security/view/';
+
+  try {
+    Records.forEach(async streamRecord => {
+      let record = streamRecord.dynamodb.NewImage;
+
+      if (record) {
+        let bond = record.starBond.S;
+        let html = await fetch(BASE_URL + bond);
+        console.log(bond);
+        console.log(html)
+      } else {
+        console.log('Record has no new image', streamRecord)
+      }
+    });
+
+    callback(null)
+  } catch (e) {
+    callback(e)
+  }
 }
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+import https from 'https'
+
+export const fetch = (url) => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, resp => {
+        const { statusCode } = resp;
+        if (statusCode !== 200) reject(`Status code: ${statusCode}`);
+
+        let data = '';
+        resp.on('data', chunk => data += chunk);
+        resp.on('end', () => resolve(data));
+      })
+      .on('error', reject)
+  })
+};
