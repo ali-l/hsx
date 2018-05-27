791c90376953889d8408c9e30eb63c63f7e30e06
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-05-26 20:49:46 -0400

    Update handler to create a job for each TAG-relevant credit and then create a job for the bond

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 import Bond from './models/Bond'
+import Job from './models/Job'
+
+const BOND = 'BOND';
+const STOCK = 'STOCK';
+
+const processBond = async (job) => {
+  console.log('processing bond', job);
+
+  let bond = await Bond.find(job.ticker);
+
+  if (job.fetchStocks) {
+    let credits = bond.creditsPotentiallyInCurrentTAG.concat(bond.creditsPotentiallyInFutureTAG);
+
+    for (let {ticker} of credits) {
+      await Job.create({ type: STOCK, ticker: ticker })
+    }
+
+    await Job.create({ type: BOND, ticker: job.ticker, fetchStocks: false })
+  } else {
+    await bond.calculateCurrentTAG();
+    await bond.save();
+  }
+};
+
+const processStock = (job) => {
+  console.log('processing stock', job)
+};
 
 // noinspection JSUnusedGlobalSymbols
-export default ({ Records }, context, callback) => {
+export default async ({ Records }, context, callback) => {
   try {
-    Records.forEach(async streamRecord => {
+    for (let streamRecord of Records) {
       let record = streamRecord.dynamodb.NewImage;
 
       if (record) {
-        let ticker = record.starBond.S;
-        let bond = await Bond.find(ticker);
-        bond.save()
+        let job = Job.fromStreamRecord(record);
+
+        if (job.type === BOND) {
+          await processBond(job)
+        } else if (job.type === STOCK) {
+          await processStock(job)
+        } else {
+          // noinspection ExceptionCaughtLocallyJS
+          throw new Error('Not a stock or bond')
+        }
+
       } else {
         console.log('Record has no new image', streamRecord)
       }
-    });
+    }
 
     callback(null)
   } catch (e) {
