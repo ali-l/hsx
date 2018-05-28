314729bef5d81e11622c216b45ad89d7fc0959b1
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-05-27 22:28:43 -0400

    Add placeholder function to process health check job

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import Bond from './models/Bond'
 import Job from './models/Job'
 
 const BOND_TYPE = 'BOND';
+const HEALTH_CHECK_TYPE = 'HEALTH_CHECK';
 
-const processBond = async ({ ticker }) => {
+async function processBond({ ticker }) {
   console.log('processing bond', ticker);
   const bond = await Bond.find(ticker);
   await bond.calculateCurrentTAG();
   await bond.save();
-};
+}
+
+async function processHealthCheck({ tickerList }) {
+  console.log('processing list', tickerList)
+}
 
 // noinspection JSUnusedGlobalSymbols
 export default async ({ Records }, context, callback) => {
@@ -20,6 +25,8 @@ export default async ({ Records }, context, callback) => {
 
     if (job.type === BOND_TYPE) {
       await processBond(job)
+    } else if (job.type === HEALTH_CHECK_TYPE) {
+      await processHealthCheck(job)
     }
   }
 
